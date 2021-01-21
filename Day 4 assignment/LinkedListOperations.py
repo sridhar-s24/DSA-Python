e1e1d7cd5b93c872c315dcd63ed9119c69d2ee3c
sridhar-s24 <Srid.2409>
2021-01-22 04:29:47 +0530

    Modified LinkedListOperations.py

--- Day 4 assignment/LinkedListOperations.py
+++ Day 4 assignment/LinkedListOperations.py
@@ -9,14 +9,14 @@ class LinkedList:
     
                 
     def displayEle(self):
-            if self.head == None:
-                print ('List is empty')  
-            else:
-                temp = self.head
-                while temp != None:
-                    print(temp.value, end = " ")
-                    temp = temp.next
-                print('\n')
+        print('\n')
+        if self.head == None:
+            print ('List is empty')  
+        else:
+            temp = self.head
+            while temp != None:
+                print(temp.value, end = " ")
+                temp = temp.next
         
     def insertFirst(self,value):
         node = Node(value)
@@ -29,6 +29,41 @@ class LinkedList:
             node.next = second
             self.head = node
         
+    def insertLast(self,value):
+        node = Node(value)
+        if self.head == None:
+            self.head = node
+        else:
+            temp = self.head
+            while temp.next != None:
+                temp = temp.next
+            temp.next = node
+    
+    def insertByIndex(self,value,index):
+            node = Node(value)
+            if self.head == None:
+                self.head = node
+                
+            elif index == 0:
+                second = self.head
+                node.next = second
+                self.head = node
+                
+            else:
+                temp = self.head
+                for i in range(index - 1):
+                    temp = temp.next
+                temp2 = temp.next
+                temp.next = node
+                node.next = temp2
+                
+            
+    def deleteFirst(self):
+            if self.head == None:
+                 print ('List is empty') 
+            else:
+                self.head = self.head.next
+    
     def deleteLast(self):
             if self.head == None:
                  print ('List is empty') 
@@ -37,6 +72,20 @@ class LinkedList:
                 while(temp.next.next != None):
                     temp = temp.next
                 temp.next = None
+    
+    def deleteByIndex(self,index):
+            if self.head == None:
+                 print ('List is empty') 
+             
+            elif index == 0:
+                self.head = self.head.next
+             
+            else:
+                temp = self.head
+                for i in range(index - 1):
+                    temp = temp.next
+                
+                temp.next = temp.next.next
     
     def eleCount(self):
             length = 0
@@ -50,6 +99,7 @@ class LinkedList:
             return length
     
     def findMidEle(self,middle):
+            print('\n')
             temp = self.head
             if self.head == None:
                 print ('List is empty')
@@ -65,14 +115,22 @@ class LinkedList:
 
 
 ll = LinkedList()
-ll.insertFirst(4)
-ll.insertFirst(3)
-ll.insertFirst(2)
-ll.insertFirst(1)
+ll.insertLast(10)
+ll.insertLast(20)
+ll.insertLast(30)
+ll.insertFirst(5)
 ll.displayEle()
 ll.deleteLast()
 ll.displayEle()
+ll.insertLast(40)
+ll.insertLast(50)
+ll.displayEle()
 count = ll.eleCount()
 ll.findMidEle(int(count/2))
-
+ll.deleteFirst()
+ll.displayEle()
+ll.insertByIndex(25,2)    
+ll.displayEle()
+ll.deleteByIndex(3)
+ll.displayEle()
         
